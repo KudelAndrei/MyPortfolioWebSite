b1bba862668493b83bbf75d8c4189a94f54cb869
KudelAndrei <kudel1995@mail.ru>
2017-02-21 01:00:07 +0300

    toggle menu

--- app/js/common.js
+++ app/js/common.js
@@ -1,12 +1,64 @@
 $(document).ready(function(){
-	$(".menu__item--about").hover(function(){
-			$(".menu__image").siblings().removeClass("hover");
-			$(".menu__image--about").addClass("hover");
-	});
-	$(".menu__item--skills").hover(function(){
-			$(".menu__image").siblings().removeClass("hover");
-			$(".menu__image--skills").addClass("hover");
 
+	toggle = function toggleMenu(){
+		if ($(this).hasClass('toggle')){
+			$(this).removeClass('toggle');
+			$('.wrap-container').removeClass('menu--active');
+		}
+	};
+
+	hover = function hoverMenu(){
+		if($(this).hasClass('menu__item--about')){
+			$('.menu__image').siblings().removeClass("hover");
+			$('.menu__image--about').addClass('hover');
+			$(".about").addClass('section--active');
+		}
+		else if ($(this).hasClass('menu__item--skills')){
+			$('.menu__image').siblings().removeClass("hover");
+			$('.menu__image--skills').addClass('hover');
+		}
+		else if ($(this).hasClass('menu__item--work')){
+			$('.menu__image').siblings().removeClass("hover");
+			$('.menu__image--work').addClass('hover');
+		}
+		else if ($(this).hasClass('menu__item--contact')){
+			$('.menu__image').siblings().removeClass("hover");
+			$('.menu__image--contact').addClass('hover');
+		}
+	};
+
+	section = function toggleSection(){
+		$(".wrap-container").addClass("menu--active");
+		$(".logo").addClass("toggle");
+		if ($(this).hasClass('menu__item--about')){
+			$('.section').removeClass('section--active');
+			$('.about').addClass('section--active');
+		}
+		else if ($(this).hasClass('menu__item--skills')){
+			$('.section').removeClass('section--active');
+			$('.skills').addClass('section--active');
+		}
+		else if ($(this).hasClass('menu__item--work')){
+			$('.section').removeClass('section--active');
+			$('.work').addClass('section--active');
+		}
+		else if ($(this).hasClass('menu__item--contact')){
+			$('.section').removeClass('section--active');
+			$('.contact').addClass('section--active');
+		}
+	}
+
+	$('.logo').click(toggle);
+	$('.menu__item').hover(hover);
+	$('.menu__item').click(section);
+
+	// закрытие меню
+	$(".toggle").click(function(){
+		$(".wrap-container").toggleClass("menu--active");
+		$(this).removeClass("toggle");
 	});
+
+
+
 });
 
